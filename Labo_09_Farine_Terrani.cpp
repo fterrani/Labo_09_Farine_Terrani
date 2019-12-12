a8cb48e39902e4a140eff20c6fa41b213ce8dc9d
Fabien Terrani <fabien.terrani@heig-vd.ch>
2019-12-12 11:23:22 +0100

    Initial commit

---
+++ Labo_09_Farine_Terrani.cpp
@@ -0,0 +1,21 @@
+/*
+ --------------------------------------------------------------------------
+ Fichier       : Labo_09_Farine_Terrani.cpp
+ Auteurs       : Kevin Farine, Terrani Fabien
+ Date          : 12.12.2019
+ 
+ But           : TODO
+ 
+ Remarque(s)   : TODO
+ 
+ Compilateur   : gcc
+ --------------------------------------------------------------------------
+ */
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+   return EXIT_SUCCESS;
+}
